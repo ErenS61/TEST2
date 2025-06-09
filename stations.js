c11a63f8c9d7306ad1f045aa65ea104cf430dc0d
ErenS61 <95621105+ErenS61@users.noreply.github.com>
2025-06-09 23:31:10 +0200

    Update stations.js

--- stations.js
+++ stations.js
@@ -1,6 +1,7 @@
 // Liste des stations disponibles
 const stations = [
     { id: "68400001", nom: "Super U Riedisheim" },
+    { id: "68000020", nom: "ENI Colmar" },
     
     // Carrefour
     { id: "68200004", nom: "Carrefour Mulhouse Dornach" },
